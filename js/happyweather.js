7871d6bb96b15f04e56c7514c27f5f31ff5db278
bionicBryce <and02027@byui.edu>
2018-04-06 17:35:38 -0600

    Summary
    making the weather happy

--- js/happyweather.js
+++ js/happyweather.js
@@ -1,3 +1,89 @@
+$(document).ready(function () {
+
+
+    $("#request").click(function () {
+
+        var locationRequestType = $("#location-choice").val();
+        var forcastRequestType = $("#forcast-choice").val();
+        var enteredLocation = $("#enter-location").val();
+
+        console.log("location request: " + locationRequestType);
+        console.log("forcast request: " + forcastRequestType);
+        console.log("entered location: " + enteredLocation);
+        console.log("---");
+
+      var targetURL = "https://api.openweathermap.org/data/2.5/"+ forcastRequestType +"?zip="+ enteredLocation +",us&appid=19e005a1538e1e345999d12124968754&units=imperial"
+
+        $.ajax({
+            url:targetURL,
+            dataType: "jsonp",
+            success: function processResults(data) {
+                // step 1 - dump results into console to see what's in the objects returned
+                console.log(data);
+// Step 2 - process results
+              $("#results").empty();
+              if (forcastRequestType == "weather") {
+                console.log("weather req");
+                $("#results").append(
+                "<h1>" + data.name +"</h1> <p>" +
+                  data.clouds.all + "% Cloud Coverage</p>"
+
+                );
+
+              }
+                  else{
+
+                   $("#results").append(
+                "<h1>" + data.city.name +"</h1>");
+
+                    for (var i=0; i < data.list.length; i++)
+
+                 $("#results").append( "<div>" +data.list[i].weather[0].description + "</div>"
+                );  }
+            }
+        });
+    });
+
+    var data;
+
+    $("#current").click(function () {
+  console.log("current click");
+
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?zip=44129,us&appid=19e005a1538e1e345999d12124968754",
+            dataType: "jsonp",
+            success: function processResults(data) {
+                // step 1 - dump results into console to see what's in the objects returned
+                console.log(data);
+
+
+            }
+
+        });
+
+    });
+
+    $("#future").click(function () {
+  console.log("future click");
+
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?zip=44129,us&appid=19e005a1538e1e345999d12124968754",
+            dataType: "jsonp",
+            success: function processResults(data) {
+                // step 1 - dump results into console to see what's in the objects returned
+                console.log(data);
+
+
+            }
+
+        });
+
+    });
+
+
+});
+
+/*
 const TempUnitC = "&deg;C";
 const TempUnitF = "&deg;F";
 //Large Background images url:
@@ -43,3 +129,4 @@ const weatherIcon = {
     thunder : "https://www.amcharts.com/wp-content/themes/amcharts2/css/img/icons/weather/animated/thunder.svg"
   }
 }
+*/
